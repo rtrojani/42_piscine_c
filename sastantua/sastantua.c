8192a8c4b789e2694a39613c55424ed9e8bf6f9d
rtrojani <romain.trojani.bigot@gmail.com>
2019-05-06 23:54:05 +0200

    Sastantua ok, porte ok, poignée ok et le tout à la norme ! :)

--- sastantua/sastantua.c
+++ sastantua/sastantua.c
@@ -5,12 +5,57 @@ int		width(int i)
 	if (i == 0)
 		return (7);
 	else
-			return(width(i - 1) + 2 * (1
-				+ (i + 3) + (i / 2 + i % 2)));
-	
+		return (width(i - 1) + 2 * (1
+			+ (i + 3) + (i / 2 + i % 2)));
+
 }
 
-void	stair(int stair_i, int size)
+void	put_ground_floor(int i, int j, int high, int size)
+{
+	int		door_size;
+
+	door_size = size + 1 - size % 2;
+	ft_putchar('/');
+	while (j < width(size) - high - 1 + i)
+	{
+		if (((i == (high - (door_size / 2) - 1)) 
+			&& (j == ((width(size) / 2) + (door_size / 2 + 1) - 3))) 
+			&& (size >= 4))
+			ft_putchar('$');
+		else if ((i > (high - door_size - 1)) 
+			&& ((j > ((width(size) / 2) - (door_size / 2 + 1) - 1)) 
+			&& ((j < (width(size) / 2) + (door_size / 2 + 1) - 1))))
+			ft_putchar('|');
+		else
+			ft_putchar('*');
+		j++;
+	}
+	ft_putchar('\\');
+}
+
+void	ground_floor(int size)
+{
+	int		high;
+	int		i;
+	int		j;
+
+	i = 0;
+	high = size + 3;
+	while (i < high)
+	{
+		j = 0;
+		while (j < (high - 1 - i))
+		{
+			ft_putchar(' ');
+			j++;
+		}
+		put_ground_floor(i, j, high, size);
+				ft_putchar('\n');
+		i++;
+	}
+}
+
+void	put_floor(int floor_i, int size)
 {
 	int		high;
 	int		gap;
@@ -18,20 +63,17 @@ void	stair(int stair_i, int size)
 	int		j;
 
 	i = 0;
-	high = stair_i + 3;
+	high = floor_i + 3;
 	while (i < high)
 	{
 		j = 0;
-		gap = (width(size - 1) - width(stair_i)) / 2;
+		gap = (width(size - 1) - width(floor_i)) / 2;
 		while (gap-- > 0)
 			ft_putchar(' ');
-		while (j < (high - 1 - i))
-		{
+		while (j++ < (high - 1 - i))
 			ft_putchar(' ');
-			j++;
-		}
 		ft_putchar('/');
-		while (j < width(stair_i) - high - 1 + i)
+		while (j < width(floor_i) - high + i)
 		{
 			ft_putchar('*');
 			j++;
@@ -47,6 +89,7 @@ void	sastantua(int size)
 	int		j;
 
 	j = 0;
-	while (j < size)
-		stair(j++, size);
+	while (j < (size - 1))
+		put_floor(j++, size);
+	ground_floor(size - 1);
 }
